0ba15c9c76ca0e4f7c4eb19aca64b246a1f53d3b
jessica-oro <jessica@oro.co>
2019-10-30 12:15:06 -0700

    Remove semicolon

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 //Local development
 //include "autoload.php";
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"))
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
 $username = $url["user"];
