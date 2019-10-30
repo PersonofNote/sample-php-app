fd302e051c5f4139d08b1e84a4c36f746fce875c
jessica-oro <jessica@oro.co>
2019-10-29 17:09:31 -0700

    Add to and display tasks from database

--- public/index.php
+++ public/index.php
@@ -17,19 +17,36 @@ ini_set('display_errors', '1');
   $DB_PASSWORD = env('DB_PASSWORD');
   $DB_NAME = env('DB_NAME');
 
-  $db = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
-
-	// insert a quote if submit button is clicked
+  $db = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME) or die("Could not connect to the database");
+  $listdbtables = array_column(mysqli_fetch_all($db->query('SHOW TABLES')),0);
+  //var_dump($listdbtables);
+  // insert a task if submit button is clicked
 	if (isset($_POST['submit'])) {
 		if (empty($_POST['task'])) {
       $errors = "You must fill in the task";
 		}else{
-			$task = $_POST['task'];
-      $sql = "INSERT INTO tasks (task) VALUES ('$task')";
-			mysqli_query($db, $sql);
-      header('location: index.php');
+      $task = $_POST['task'];
+      $task_duration = $_POST['task_duration'];
+      $query = "INSERT INTO tasks (task, amount_of_time) 
+  			  VALUES('$task', '$task_duration')";
+      $result = mysqli_query($db, $query);
+      //var_dump($result);
+      if ($result) {
+        header('location: index.php');
+    } else {
+        echo ("Could not insert data. ");
+    }
+     
 		}
   }
+
+  //delete
+if (isset($_GET['del_task'])) {
+	$id = $_GET['del_task'];
+
+	mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+	header('location: index.php');
+}
   
 
   //User login and session info
@@ -74,8 +91,8 @@ ini_set('display_errors', '1');
   <form method="post" action="index.php" class="input_form">
     <label for="task"> What do you want to do? </label>
     <input type="text" name="task" class="task_input">
-    <label for="task_time"> For how many minutes? </label>
-    <input type="number" name="task_time" class="task_input">
+    <label for="task_duration"> For how many minutes? </label>
+    <input type="number" name="task_duration" class="task_input">
     <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
   </form>
   <table>
@@ -87,27 +104,23 @@ ini_set('display_errors', '1');
 
     <tbody>
       <?php 
-		// select all tasks if page is visited or refreshed
+    // select all tasks if page is visited or refreshed
     $tasks = mysqli_query($db, "SELECT * FROM tasks")
-      or die("Error: ".mysqli_error($db));
-    $array = mysqli_fetch_array($tasks); ?>
+      or die("Error: ".mysqli_error($db)); ?>
 
-    <div class="TESTARRAY">
-    <?php echo $array; ?>
-    </div>
-     <?php 
-     $i = 0; while ($i < 5) { ?>
-    <tr>
-      <td class="task"> Lorem </td>
-      <td class="delete">
-        <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a>
-      </td>
-    </tr>
-    <?php $i++; } ?>
-   <?php //Loop over and display tasks
-		$i = 0; while ($row = mysqli_fetch_array($tasks)) { ?>
+      <div class="no-results">
+        <?php 
+    //echo mysqli_num_rows($tasks);
+    if(mysqli_num_rows($tasks) < 1) {
+      echo "No tasks found";
+    }
+    ?>
+      </div>
+      <?php //Loop over and display tasks
+		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
       <tr>
-        <td class="task"> <?php echo $row['task']; ?> </td>
+        <td class="task"> <?php echo $row['task'] . ": "; ?> </td>
+        <td class="duration"> <?php echo $row['amount_of_time'] . " minutes"; ?> </td>
         <td class="delete">
           <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a>
         </td>
@@ -116,4 +129,5 @@ ini_set('display_errors', '1');
     </tbody>
   </table>
 </div>
+
 <?php include "templates/footer.php"; ?>
