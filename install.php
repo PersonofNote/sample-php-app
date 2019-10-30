75f689f8d1d96913796d1b6a73067f428a60358c
jessica-oro <jessica@oro.co>
2019-10-30 12:03:22 -0700

    Fix msql->msqli

--- install.php
+++ install.php
@@ -9,11 +9,11 @@
 require "config.php";
 
 try {
-  $connection = new PDO("mysql:host=$host", $username, $password, $options);
+  $connection = new PDO("mysqli:host=$host", $username, $password, $options);
   $sql = file_get_contents("data/init.sql");
   $connection->exec($sql);
 
-  echo "Database and table users created successfully.";
+  echo "Database and tables created successfully.";
 } catch(PDOException $error) {
   echo $sql . "<br>" . $error->getMessage();
 }
