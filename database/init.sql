dd535a3defb00fee3ba457784788111b9a39de61
jessica-oro <jessica@oro.co>
2019-10-30 15:12:07 -0700

    Update database init

--- database/init.sql
+++ database/init.sql
@@ -1,15 +1,17 @@
-use cleardb-graceful-17842;
+CREATE DATABASE IF NOT EXISTS cleardb-graceful-17842;
 
-CREATE TABLE users (
-userid INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(45) NOT NULL,
-email VARCHAR(80) NOT NULL,
-password VARCHAR(50),
-);
+  use cleardb-graceful-17842;
 
-CREATE TABLE tasks (
-taskid INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-task VARCHAR(255) NOT NULL,
-duration TINYINT(4) NOT NULL,
-username VARCHAR(45),
-);
+  CREATE TABLE users (
+    userid INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(45) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    password VARCHAR(50),
+  );
+
+  CREATE TABLE tasks (
+    taskid INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    task VARCHAR(255) NOT NULL,
+    duration TINYINT(4) NOT NULL,
+    username VARCHAR(45),
+  );
