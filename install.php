60c659b84088db7a5656733e0532e135c779c191
jessica-oro <jessica@oro.co>
2019-10-30 12:22:23 -0700

    Consolidate files

--- install.php
+++ install.php
@@ -1,13 +1,26 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+
+
+$env_var = getenv("CLEARDB_DATABASE_URL");
+$url = parse_url($env_var);
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$dsn        = "mysqli:host=$host;dbname=$db";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
 /**
   * Open a connection via PDO to create a
   * new database and table with structure.
   *
   */
 
-require "config.php";
+//require "config.php";
 
 try {
   $connection = new PDO("mysqli:host=$host", $username, $password, $options);
