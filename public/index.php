06cb1fc7fdcabca657db438f912d196155a3ba17
jessica-oro <jessica@oro.co>
2019-10-30 09:23:05 -0700

    SELECT posts from user (need to learn prepared statements for variable insertion)

--- public/index.php
+++ public/index.php
@@ -1,53 +1,6 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-//Load environment variables
-  include "../autoload.php";
-
-  /** Task submission info 
-   * 
-   *  TODO: DRY out (currently repeating some code from server.php)
-   * 
-  */
-  $errors = "";
-
-  //database environment variables
-  $DB_HOST = env('DB_HOST');
-  $DB_USERNAME = env('DB_USERNAME');
-  $DB_PASSWORD = env('DB_PASSWORD');
-  $DB_NAME = env('DB_NAME');
-
-  $db = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME) or die("Could not connect to the database");
-  $listdbtables = array_column(mysqli_fetch_all($db->query('SHOW TABLES')),0);
-  //var_dump($listdbtables);
-  // insert a task if submit button is clicked
-	if (isset($_POST['submit'])) {
-		if (empty($_POST['task'])) {
-      $errors = "You must fill in the task";
-		}else{
-      $task = $_POST['task'];
-      $task_duration = $_POST['task_duration'];
-      $query = "INSERT INTO tasks (task, amount_of_time) 
-  			  VALUES('$task', '$task_duration')";
-      $result = mysqli_query($db, $query);
-      //var_dump($result);
-      if ($result) {
-        header('location: index.php');
-    } else {
-        echo ("Could not insert data. ");
-    }
-     
-		}
-  }
-
-  //delete
-if (isset($_GET['del_task'])) {
-	$id = $_GET['del_task'];
-
-	mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
-	header('location: index.php');
-}
-  
 
   //User login and session info
   session_start(); 
@@ -81,12 +34,56 @@ if (isset($_GET['del_task'])) {
   <?php endif ?>
 
   <!-- logged in user information -->
-  <?php  if (isset($_SESSION['username'])) : ?>
+  <?php  if (isset($_SESSION['username'])) :
+      $thisuser = $_SESSION['username']; //TODO change to use ID instead of username to join tables
+      //Load environment variables
+  include "../autoload.php";
+
+  /** Task submission info 
+   * 
+   *  TODO: DRY out (currently repeating some code from server.php)
+   * 
+  */
+  $errors = "";
+
+  //database environment variables
+  $DB_HOST = env('DB_HOST');
+  $DB_USERNAME = env('DB_USERNAME');
+  $DB_PASSWORD = env('DB_PASSWORD');
+  $DB_NAME = env('DB_NAME');
+
+  $db = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME) or die("Could not connect to the database");
+  $listdbtables = array_column(mysqli_fetch_all($db->query('SHOW TABLES')),0); //For debugging
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['task'])) {
+      $errors = "You must fill in the task";
+		}else{
+      $task = $_POST['task'];
+      $task_duration = $_POST['task_duration'];
+      $query = "INSERT INTO tasks (task, duration, username) 
+  			  VALUES('$task', '$task_duration', '$thisuser')";
+      $result = mysqli_query($db, $query);
+      if ($result) {
+        header('location: index.php');
+    } else {
+        echo ("Could not insert data. ");
+    }
+		}
+  }
+
+  //delete
+if (isset($_GET['del_task'])) {
+	$id = $_GET['del_task'];
+
+	mysqli_query($db, "DELETE FROM tasks WHERE taskid=".$id);
+	header('location: index.php');
+}
+     ?>
   <?php if (isset($errors)) { ?>
   <p><?php echo $errors; ?></p>
   <?php } ?>
   <?php endif ?>
-  <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+  <p>Welcome <strong><?php echo $thisuser; ?></strong></p>
   <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
   <form method="post" action="index.php" class="input_form">
     <label for="task"> What do you want to do? </label>
@@ -105,7 +102,8 @@ if (isset($_GET['del_task'])) {
     <tbody>
       <?php 
     // select all tasks if page is visited or refreshed
-    $tasks = mysqli_query($db, "SELECT * FROM tasks")
+   // $tasks = mysqli_query($db, "SELECT * FROM tasks")
+    $tasks = mysqli_query($db, "SELECT * FROM users JOIN tasks ON users.username = tasks.username WHERE users.username = 'jessie'")
       or die("Error: ".mysqli_error($db)); ?>
 
       <div class="no-results">
@@ -120,9 +118,10 @@ if (isset($_GET['del_task'])) {
 		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
       <tr>
         <td class="task"> <?php echo $row['task'] . ": "; ?> </td>
-        <td class="duration"> <?php echo $row['amount_of_time'] . " minutes"; ?> </td>
+        <td class="duration"> <?php echo ($row['duration']) . " minutes"; ?> </td>
+        <td class="duration"> <?php echo $row['username'] . " user"; ?> </td>
         <td class="delete">
-          <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a>
+          <a href="index.php?del_task=<?php echo $row['taskid'] ?>">x</a>
         </td>
       </tr>
       <?php $i++; } ?>
