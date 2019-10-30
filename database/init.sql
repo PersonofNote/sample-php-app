32455f60d531c688731173332bfc954628cbed92
jessica-oro <jessica@oro.co>
2019-10-30 15:09:48 -0700

    Update database info

--- database/init.sql
+++ database/init.sql
@@ -1,17 +1,15 @@
-CREATE DATABASE IF NOT EXISTS task_app_db;
+use cleardb-graceful-17842;
 
-  use task_app_db;
+CREATE TABLE users (
+userid INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(45) NOT NULL,
+email VARCHAR(80) NOT NULL,
+password VARCHAR(50),
+);
 
-  CREATE TABLE users (
-    userid INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(45) NOT NULL,
-    email VARCHAR(80) NOT NULL,
-    password VARCHAR(50),
-  );
-
-  CREATE TABLE tasks (
-    taskid INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    task VARCHAR(255) NOT NULL,
-    duration TINYINT(4) NOT NULL,
-    username VARCHAR(45),
-  );
+CREATE TABLE tasks (
+taskid INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+task VARCHAR(255) NOT NULL,
+duration TINYINT(4) NOT NULL,
+username VARCHAR(45),
+);
